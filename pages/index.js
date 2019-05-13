5b6b5fd366ea4798eb2a8edc0383b2ecf6a63787
Tatiana <tatianaveratti@gmail.com>
2019-05-12 19:05:30 -0500

    initial home

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Arena Project Testing</h1>
+      <div>Updated: May 12, 2019</div>
+      <a href="./static/arena.zip">Download .zip</a>
+    </div>
+  );
+}
+
+export default Home;
