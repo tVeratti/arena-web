9757035b101c0bb5e5b1afd4b5a67e01371e1cb5
Tatiana <tatianaveratti@gmail.com>
2019-05-22 02:19:46 -0500

    update exe

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="home">
       <h1>Arena Project Testing</h1>
-      <div>Updated: May 12, 2019</div>
+      <div>Updated: May 22, 2019</div>
       <a href="./static/arena.zip">Download .zip</a>
     </div>
   );
