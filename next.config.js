61c487ced5c3bf09943421572232f20b85a26fc5
Tatiana <tatianaveratti@gmail.com>
2019-05-28 20:44:22 -0500

    revert next.config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,2 @@
-module.exports = {
-  webpack: config => {
-    // config.module.rules.push({
-    //   test: /\.js$/,
-    //   exclude: /(node_modules)/,
-    //   use: {
-    //     loader: 'babel-loader'
-    //   }
-    // });
-
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader'
-    });
-
-    // config.module.rules.push({
-    //   test: /\.scss$/,
-    //   use: 'sass-loader'
-    // });
-
-    return config;
-  }
-};
+const withSass = require('@zeit/next-sass');
+module.exports = withSass();
