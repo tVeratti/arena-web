f5147f7a803a15d41fab3f2f18a45cde0099b12b
Tatiana <tatianaveratti@gmail.com>
2019-05-28 20:39:57 -0500

    initial md loading, new version

--- next.config.js
+++ next.config.js
@@ -1,2 +1,23 @@
-const withSass = require('@zeit/next-sass');
-module.exports = withSass();
+module.exports = {
+  webpack: config => {
+    // config.module.rules.push({
+    //   test: /\.js$/,
+    //   exclude: /(node_modules)/,
+    //   use: {
+    //     loader: 'babel-loader'
+    //   }
+    // });
+
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader'
+    });
+
+    // config.module.rules.push({
+    //   test: /\.scss$/,
+    //   use: 'sass-loader'
+    // });
+
+    return config;
+  }
+};
